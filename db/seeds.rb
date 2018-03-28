939fb524d1f54c23fb899aa4d17de1eb995e8187
Toan Nguyen <ntuantoan@gmail.com>
2018-03-28 20:05:12 +0700

    Index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
 # Create blog posts
-10.times do |i|
+10.times do
   Blog.create! title: Faker::Lorem.sentence,
                body:  Faker::Lorem.paragraph(10)
 end
 puts "10 blog posts created!"
 
 # Create skills
-5.times do |i|
-  Skill.create! title:            Faker::Lorem.word,
+5.times do
+  Skill.create! title:            Faker::Lorem.word.capitalize,
                 percent_utilized: 20
 end
 puts "5 skills created!"
 
 # Create portfolio items
-9.times do |i|
-  Portfolio.create! title:       Faker::Lorem.word,
+9.times do
+  Portfolio.create! title:       Faker::Lorem.word.capitalize,
                     body:        Faker::Lorem.paragraph(4),
-                    subtitle:    Faker::Lorem.sentence ,
+                    subtitle:    Faker::Lorem.sentence,
                     main_image:  "https://via.placeholder.com/600x400",
                     thumb_image: "https://via.placeholder.com/300x200"
 end
