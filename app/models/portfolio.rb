825633008fd55dfb8aa9e906efadf8305542f0a7
Toan Nguyen <ntuantoan@gmail.com>
2018-04-01 07:29:46 +0700

    Implemented placeholder concern for generating default images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :angular,       ->{where subtitle: "Angular"}
@@ -9,7 +11,7 @@ class Portfolio < ApplicationRecord
   private
 
   def set_default_images
-    self.main_image  ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/300x200"
+    self.main_image  ||= Placeholder.image_generator width: 600, height: 400
+    self.thumb_image ||= Placeholder.image_generator width: 300, height: 200
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,13 @@
 class Skill < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :percent_utilized
+
+  after_initialize :set_default_badge
+
+  private
+
+  def set_default_badge
+    self.badge = Placeholder.image_generator width: 250, height: 250
+  end
 end
