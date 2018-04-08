4c9ab1bc22b54590adf6d7746a7e8d37a0b49d14
Toan Nguyen <ntuantoan@gmail.com>
2018-04-08 16:20:53 +0700

    Added navbar, page title, and SEO keywords

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
   def guest_user
     OpenStruct.new name: "Guest", first_name: "Guest", last_name: "Guest"
   end
+
+  # View helper: Return title for a page
+  def page_title title = ""
+    title.empty? ? "Dissect" : title
+  end
+
+  # View helper: Return SEO keywords for a page
+  def seo_keywords keywords = ""
+    keywords.empty? ? "Rails 5 sample app" : keywords
+  end
 end
