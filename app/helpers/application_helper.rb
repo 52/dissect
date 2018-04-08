0a75494f504a65cc9c96fcd13eb2e123a3b31166
Toan Nguyen <ntuantoan@gmail.com>
2018-04-08 15:19:32 +0700

    Overwrited current_user, devise helper method, using Null Object Design Pattern

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Overwrite `current_user` helper of devise
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new name: "Guest", first_name: "Guest", last_name: "Guest"
+  end
 end
