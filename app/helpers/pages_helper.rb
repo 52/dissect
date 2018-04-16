14e572f22cc7832f90d0d46e06bdd4d1c35f3655
Toan Nguyen <ntuantoan@gmail.com>
2018-04-16 14:26:55 +0700

    Made links in tweets clickable

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,26 @@
 module PagesHelper
+  # Make link in tweets clickable
+  def tweet_parser tweet
+    link_regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(link_regex) do |url|
+      "<a href='#{url}' tatget='_blank'> #{url} </a>"
+    end.html_safe
+  end
 end
